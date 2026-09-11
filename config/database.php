58752c46d8a811228d465e1fae1684d7f754e115
Witsanu Sriyota <witsanu.ict@gmail.com>
2026-09-11 13:09:31 +0700

    Revert max_execution_time from PDO init command for MariaDB compatibility

--- config/database.php
+++ config/database.php
@@ -33,8 +33,8 @@ class Database {
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES   => false,
-            PDO::ATTR_TIMEOUT            => 2, // 2 seconds connection timeout for fast failover
-            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8, time_zone = '+07:00', max_execution_time = 2000"
+            PDO::ATTR_TIMEOUT            => 3, // 3 seconds connection timeout for fast failover
+            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8, time_zone = '+07:00'"
         ];
 
         try {
